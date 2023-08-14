3d6a47b5bc31d7f73ae70fd2fbc804a8f82fc32d
mbubula6 <mbubula6@gmail.com>
2023-08-15 01:26:13 +0200

    tried with rounds

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ function setup() {
     let numberOfPlayers = prompt("Insert number of players",);
     let players = [];
     let winningAmount = prompt("Set the winning amount of points",);
+    let input = document.querySelector("#input");
+    let btn = document.querySelector("#btn");
 
-    document.getElementsByTagName("main")[0].innerHTML = "<h2>Scores</h2><table><tr id='scoreTableHeader'><th>Round</th>";
+
+    document.getElementsByTagName("main")[0].innerHTML = "<h2>Scores</h2><table id='scoreTable'><tr id='scoreTableHeader'><th>Round</th>";
     
     for ( i = 0; i < numberOfPlayers; i++ ) {
         players[i] = prompt("Insert Player " + (i+1) + " name");
@@ -15,6 +18,24 @@ function setup() {
     document.getElementsByTagName("main").innerHTML += "</tr></table>";
 }
 
+function rounds() {
+
+    for ( let round = 1; round < 3; round++) {
+        document.getElementById("scoreTable").innerHTML += "<tr>";
+
+        for ( let i = 0; i < numberOfPlayers; i++ ) {   
+            btn.addEventListener("click", () => {
+                // players[i][round] = input.value;  
+                document.getElementById("scoreTable").innerHTML += "<td>" + 4 + "</td>";
+            });   
+
+        }
+        document.getElementById("scoreTable").innerHTML += "</tr>"
+    }
+    
+    //document.getElementById("scoreTable").innerHTML = "<th>" + numberOfPlayers + "</th>";
+
+}
 
 
 
