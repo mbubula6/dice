5cfa5c4ae1fc4259c57d6bf98be503d38416f742
mbubula6 <mbubula6@gmail.com>
2023-08-15 00:37:57 +0200

    setup() of the game made

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let numberOfPlayers = prompt("Insert number of players",);
-let players = [];
-let winningAmount = prompt("Set the winning amount of points",);
+function setup() {
+    let numberOfPlayers = prompt("Insert number of players",);
+    let players = [];
+    let winningAmount = prompt("Set the winning amount of points",);
 
-for ( i = 0; i < numberOfPlayers; i++ ) {
-    players[i] = prompt("Insert Player " + (i+1) + " name");
-    document.getElementById("scoreTableHeader").innerHTML += ("<th>" + players[i] + "</th>");
+    document.getElementsByTagName("main")[0].innerHTML = "<h2>Scores</h2><table><tr id='scoreTableHeader'><th>Round</th>";
+    
+    for ( i = 0; i < numberOfPlayers; i++ ) {
+        players[i] = prompt("Insert Player " + (i+1) + " name");
+        document.getElementById("scoreTableHeader").innerHTML += ("<th>" + players[i] + "</th>");
+    }
+
+
+
+    document.getElementsByTagName("main").innerHTML += "</tr></table>";
 }
-
 
 
 
