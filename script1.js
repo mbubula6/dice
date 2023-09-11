1cf206ea7c7245040a930be06aa66c9f58824ba9
mbubula6 <142213441+mbubula6@users.noreply.github.com>
2023-09-11 12:35:34 +0200

    create new script file (script1.js)

---
+++ script1.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    
+    const setup1 = document.querySelector(".setup1");
+
+    setup1.addEventListener("click", () =>  {
+        
+        let numberOfPlayers = prompt("Insert number of players",);
+
+        for ( i = 0; i = numberOfPlayers; i ++) {
+
+        })
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+});
