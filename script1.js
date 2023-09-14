926d25389c6c55c9d5fb6cf9568b34f574e76f5c
mbubula6 <mbubula6@gmail.com>
2023-09-14 17:14:54 +0200

    Fixed the problem with the table(in script1.js): made the rows appear and the button disappear after finishing the setup1

--- script1.js
+++ script1.js
@@ -1,24 +1,51 @@
     alert("huh?");
     
     const setup1 = document.getElementById("setup1");
+    const round = document.getElementById("round");
+    const table = document.getElementById("table");
+    let roundNum = 0;
+    let numberOfPlayers;
+
 
     setup1.addEventListener("click", () => {
-        
-        let numberOfPlayers = prompt("Insert number of players",);
+        numberOfPlayers = prompt("Insert number of players",);
         let playerName = [];
-
-        const table = document.getElementById("table");
 
         const newRow = document.createElement("tr");
         table.appendChild(newRow);
 
+        const cell = document.createElement("th");
+            newRow.appendChild(cell);
+            cell.insertAdjacentText("beforeend", "Round");
+
         for ( row = 0; row < numberOfPlayers; row++) {
-            playerName[row] = prompt("Insert player " + row + " name:", );
-            const newCell = document.createElement("th");
-            newRow.appendChild(newCell);
-            newCell.insertAdjacentText("beforeend", playerName[row]);
- 
+            playerName[row] = prompt("Insert player " + (row+1) + " name:", );
+            const cell = document.createElement("th");
+            newRow.appendChild(cell);
+            cell.insertAdjacentText("beforeend", playerName[row]);
+        }
+       
+        setup1.remove();
+    });
+
+    round.addEventListener("click", () => {
+        roundNum++;
+        const newRow = document.createElement("tr");
+        table.appendChild(newRow);
+
+        const cell = document.createElement("td");
+            newRow.appendChild(cell);
+            cell.insertAdjacentText("beforeend", roundNum);
+
+        for ( i = 0; i < numberOfPlayers; i++) {
+            const cell = document.createElement("td");
+            newRow.appendChild(cell);
+
+
         }
 
 
+
     });
+
+   
