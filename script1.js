0210a3313445632bb8bc548730b35199cf2983e8
mbubula6 <mbubula6@gmail.com>
2023-09-26 21:09:55 +0200

    EventListeners are being added to all of the inputs; changing the value of the inputs change color of the backround

--- script1.js
+++ script1.js
@@ -1,52 +1,61 @@
-    alert("huh?");
-    
-    const setup1 = document.getElementById("setup1");
-    const round = document.getElementById("round");
-    const table = document.getElementById("table");
-    let roundNum = 0;
-    let numberOfPlayers;
+alert("huh?");
+
+const setup1 = document.getElementById("setup1");
+const round = document.getElementById("round");
+const table = document.getElementById("table");
+const body = document.querySelector("body");
+
+let roundNum = 0;
+let numberOfPlayers;
 
 
-    setup1.addEventListener("click", () => {
-        numberOfPlayers = prompt("Insert number of players",);
-        let playerName = [];
+setup1.addEventListener("click", () => {
+    numberOfPlayers = prompt("Insert number of players",);
+    let playerName = [];
 
-        const newRow = document.createElement("tr");
-        table.appendChild(newRow);
+    const newRow = document.createElement("tr");
+    table.appendChild(newRow);
 
+    const cell = document.createElement("th");
+        newRow.appendChild(cell);
+        cell.insertAdjacentText("beforeend", "Round");
+
+    for ( row = 0; row < numberOfPlayers; row++) {
+        playerName[row] = prompt("Insert player " + (row+1) + " name:", );
         const cell = document.createElement("th");
-            newRow.appendChild(cell);
-            cell.insertAdjacentText("beforeend", "Round");
+        newRow.appendChild(cell);
+        cell.insertAdjacentText("beforeend", playerName[row]);
+    }
+    
+    setup1.remove();
+});
 
-        for ( row = 0; row < numberOfPlayers; row++) {
-            playerName[row] = prompt("Insert player " + (row+1) + " name:", );
-            const cell = document.createElement("th");
-            newRow.appendChild(cell);
-            cell.insertAdjacentText("beforeend", playerName[row]);
-        }
-       
-        setup1.remove();
-    });
+round.addEventListener("click", () => {
+    roundNum++;
+    const newRow = document.createElement("tr");
+    table.appendChild(newRow);
 
-    round.addEventListener("click", () => {
-        roundNum++;
-        const newRow = document.createElement("tr");
-        table.appendChild(newRow);
+    const cell = document.createElement("td");
+        newRow.appendChild(cell);
+        cell.insertAdjacentText("beforeend", roundNum);
 
+    for ( i = 0; i < numberOfPlayers; i++) {
         const cell = document.createElement("td");
-            newRow.appendChild(cell);
-            cell.insertAdjacentText("beforeend", roundNum);
+        newRow.appendChild(cell);
+        const input = document.createElement("input");
+        cell.appendChild(input);
 
-        for ( i = 0; i < numberOfPlayers; i++) {
-            const cell = document.createElement("td");
-            newRow.appendChild(cell);
-            const input = document.createElement("input");
-            cell.appendChild(input);
+    }
 
-        }
-
-
-
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+        input.addEventListener("input", () => {
+        
+            if ( input.value == "") {
+                body.style.backgroundColor = "red";
+            } else {
+                body.style.backgroundColor = "blue";
+            }
+        });
     });
-
-   
+});
